c62bd4101b42d1530e150e324a1efaa4dee4ed9b
Piotr Romanowski <wsrh456@gmail.com>
2021-02-04 15:31:00 +0100

    [Api] Migrate DB in Api entry point

--- src/Sher.Api/Program.cs
+++ src/Sher.Api/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Sher.Infrastructure.Data;
 
 namespace Sher.Api
 {
@@ -14,7 +17,13 @@ namespace Sher.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            context.Database.Migrate();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
