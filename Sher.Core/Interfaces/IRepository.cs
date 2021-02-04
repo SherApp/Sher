cf85be244e80027c65b3a16444d40d6471ac8921
Piotr Romanowski <wsrh456@gmail.com>
2021-02-04 11:30:33 +0100

    [Infrastructure/Core] Implement Repository pattern

---
+++ Sher.Core/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NSpecifications;
+using Sher.Core.Entities;
+
+namespace Sher.Core.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public Task<T> GetByIdAsync(Guid id);
+
+        public Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification);
+
+        public Task<IReadOnlyList<T>> ListAsync();
+
+        public Task<T> FirstOrDefaultAsync(ISpecification<T> specification);
+
+        public Task<T> AddAsync(T entity);
+
+        public Task UpdateAsync(T entity);
+
+        public Task<T> RemoveAsync(T entity);
+    }
+}
---
+++ Sher.Infrastructure/Data/Repositories/EfRepository.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NSpecifications;
+using Sher.Core.Entities;
+using Sher.Core.Interfaces;
+
+namespace Sher.Infrastructure.Data.Repositories
+{
+    public class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _dbContext;
+        private DbSet<T> Set => _dbContext.Set<T>();
+
+        public EfRepository(AppDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await Set.FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification)
+        {
+            return await Set.Where(specification.Expression).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync()
+        {
+            return await Set.ToListAsync();
+        }
+
+        public async Task<T> FirstOrDefaultAsync(ISpecification<T> specification)
+        {
+            return await Set.FirstOrDefaultAsync(specification.Expression);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            var result = await Set.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return result.Entity;
+        }
+        
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<T> RemoveAsync(T entity)
+        {
+            var result = Set.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return result.Entity;
+        }
+    }
+}
