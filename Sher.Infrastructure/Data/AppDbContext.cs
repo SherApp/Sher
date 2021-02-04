547fa0e3f4022989702bb55b85d04d54b7da35df
Piotr Romanowski <wsrh456@gmail.com>
2021-02-04 11:50:43 +0100

    [Infrastructure] Add a constructor taking options to AppDbContext

--- Sher.Infrastructure/Data/AppDbContext.cs
+++ Sher.Infrastructure/Data/AppDbContext.cs
@@ -1,10 +1,17 @@
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Sher.Core.Entities.FileAggregate;
 
 namespace Sher.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<File> Files { get; private set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
