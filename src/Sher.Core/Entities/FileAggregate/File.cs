24e85d54ace22117fab827903b56e9553676eb0f
Piotr Romanowski <wsrh456@gmail.com>
2021-02-06 19:04:11 +0100

    Add Status to File

--- src/Sher.Core/Entities/FileAggregate/File.cs
+++ src/Sher.Core/Entities/FileAggregate/File.cs
@@ -6,11 +6,23 @@ namespace Sher.Core.Entities.FileAggregate
     {
         public string Slug { get; private set; }
         public string OriginalFileName { get; private set; }
+        public FileStatus Status { get; private set; } = FileStatus.Uploaded;
 
         public File(Guid id, string slug, string originalFileName) : base(id)
         {
             Slug = slug ?? throw new ArgumentNullException(nameof(slug));
             OriginalFileName = originalFileName ?? throw new ArgumentNullException(nameof(originalFileName));
         }
+
+        public void MarkAsProcessed()
+        {
+            Status = FileStatus.Processed;
+        }
+    }
+
+    public enum FileStatus
+    {
+        Uploaded,
+        Processed
     }
 }
---
+++ src/Sher.Infrastructure/Migrations/20210206175826_AddStatusToFile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sher.Infrastructure.Migrations
+{
+    public partial class AddStatusToFile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Files",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Files");
+        }
+    }
+}
