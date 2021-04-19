91a79d71c76dd14f1c2590c9c3c6c3b5e43ca0e4
Piotr Romanowski <wsrh456@gmail.com>
2021-04-19 17:27:32 +0200

    Update Read Model on DirectoryCreatedEvent

---
+++ src/Sher.Core/Files/DirectoryCreatedEvent.cs
@@ -0,0 +1,21 @@
+using System;
+using Sher.Core.Base;
+
+namespace Sher.Core.Files
+{
+    public class DirectoryCreatedEvent : BaseDomainEvent
+    {
+        public Guid DirectoryId { get; }
+        public Guid? ParentDirectoryId { get; }
+        public Guid UploaderId { get; }
+        public string Name { get; }
+
+        public DirectoryCreatedEvent(Guid directoryId, Guid? parentDirectoryId, Guid uploaderId, string name)
+        {
+            DirectoryId = directoryId;
+            ParentDirectoryId = parentDirectoryId;
+            UploaderId = uploaderId;
+            Name = name;
+        }
+    }
+}
